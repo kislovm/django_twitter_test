3f03863e879c4758c5f6e62e313733a48f33850c
kislovm <kislovm@gmail.com>
2012-04-19 21:46:23 +0400

    oauth turned off

--- futureColors/futureColors/twitter/models.py
+++ futureColors/futureColors/twitter/models.py
@@ -10,9 +10,9 @@ class timeline():
     timeline = []
 
     def __init__(self, screen_name):
-        auth = tweepy.OAuthHandler('cgG6YUitlpof3M4mpamIg', 'wV9N6FDh7USQO5VzvIJRbCqrhH4nw6511SWxEhSacM')
-        auth.set_access_token('57903901-8MS4j9Pb3nenaKGV3CvZP8Pu9ocwOqV6JK1jAobTL', '7Leu3whjGtfLzWfJXDGyHeJscOPVf5bTkehx5ZZyDzc')
-        self.api = tweepy.API(auth)
+        #auth = tweepy.OAuthHandler('cgG6YUitlpof3M4mpamIg', 'wV9N6FDh7USQO5VzvIJRbCqrhH4nw6511SWxEhSacM')
+        #auth.set_access_token('57903901-8MS4j9Pb3nenaKGV3CvZP8Pu9ocwOqV6JK1jAobTL', '7Leu3whjGtfLzWfJXDGyHeJscOPVf5bTkehx5ZZyDzc')
+        self.api = tweepy.API()
         for status in Cursor(self.api.user_timeline, screen_name=screen_name).items():
             status = self.getMentions(status) 
             self.timeline.append(status)
